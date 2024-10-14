c4458d9f19f3ca423476f0039983e3fd063a6417
lucas-de-lima <lucasramosdelima@gmail.com>
2024-10-14 20:50:39 -0300

    fix: remove import

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { HymnProvider } from "./contexts/HymnContext";
 import Navigation from "./navigation/Navigation";
-import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
