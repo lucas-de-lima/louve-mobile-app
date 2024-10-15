6969ffbebc1c88f52fceb1aad7b6c6f34b0a470b
lucas-de-lima <lucasramosdelima@gmail.com>
2024-10-15 09:59:43 -0300

    feat: transition screen

--- app/navigation/Navigation.tsx
+++ app/navigation/Navigation.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../pages/HomeScreen/HomeScreen";
 import HymnDetailScreen from "../pages/HymnDetailScreen/HymnDetailScreen";
+import { TransitionPresets } from "@react-navigation/stack";
 
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
-    <Stack.Navigator initialRouteName="HomeScreen">
+    <Stack.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{
+        gestureEnabled: true,
+        ...TransitionPresets.SlideFromRightIOS.transitionSpec,
+      }}
+    >
       <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
       <Stack.Screen
         name="HymnDetailScreen"
